6105a9dbcf6fd2018d83bdea100b51e8b31592e3
Alex Fang <alexfang@heytz.com>
2017-05-04 21:01:01 +0800

    finish android version

--- www/ServiceWrapper.js
+++ www/ServiceWrapper.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
-exports.initService = function (host, port, topic, success, error) {
-  exec(success, error, "ServiceWrapper", "initService", [host, port, topic]);
+exports.initService = function (host, port, topic, username, password, success, error) {
+  exec(success, error, "ServiceWrapper", "initService", [host, port, topic, username, password]);
 };
 exports.stopService = function (success, error) {
   exec(success, error, "ServiceWrapper", "stopService", []);
